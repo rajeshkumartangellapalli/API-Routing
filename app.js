2c8b2629d94f80e735f0ca9c9e315db1cae86712
Rajesh Kumar <trajeshkumar37@gmail.com>
2022-08-11 03:50:15 +0000

    API Routing

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+module.exports = app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+module.exports = app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+
+app.listen(3000);
